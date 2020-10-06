d2caab829b6a2c4d6c165aa1a4956318619299ff
BenHall-7 <benjaminjahall@gmail.com>
2020-10-06 10:48:24 -0400

    Solve 3 problems so far

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,11 +2,36 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# def product_of_all_other_numbers(arr):
+#     # Simple solution involving division and no allocation
+#     product = 1
+#     for v in arr:
+#         product *= v
+    
+#     for i in range(len(arr)):
+#         arr[i] = product / arr[i]
+
+#     return arr
+
 def product_of_all_other_numbers(arr):
-    # Your code here
+    # product without division
+    left = [1] * len(arr)
+    right = [1] * len(arr)
 
-    pass
-
+    p = 1
+    for i in range(len(arr)):
+        left[i] = p
+        p *= arr[i]
+    
+    p = 1
+    for i in reversed(range(len(arr))):
+        right[i] = p
+        p *= arr[i]
+    
+    for i in range(len(arr)):
+        arr[i] = left[i] * right[i]
+    
+    return arr
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
